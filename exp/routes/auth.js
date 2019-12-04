f8d927719bd09d71728f247d0a9988acb818c7a5
Seongyu Moon <kakao0228@mju.ac.kr>
2019-12-04 11:36:54 +0900

    user part complete

--- exp/routes/auth.js
+++ exp/routes/auth.js
@@ -3,7 +3,7 @@ module.exports = (app, passport) => {
     res.render('login');
   });
 
-  app.post('/login', passport.authenticate('local-login', {
+  app.post('/login', passport.authenticate('local-signin', {
     successRedirect : '/', // redirect to the secure profile section
     failureRedirect : '/login', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
